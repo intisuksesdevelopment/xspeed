ed39ac144f9473fbfa11c506b06e60e6b8761022
reza <rezadionugraha@gmail.com>
2025-02-11 09:57:10 +0700

    [FIX][FEATURE]login

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,15 +1,13 @@
 <?php
-
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
@@ -57,21 +55,32 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
-            'password' => 'hashed',
+            'password'          => 'hashed',
         ];
     }
 
     public function validateAttributes($attributes)
     {
         $validator = Validator::make($attributes, [
-            'uuid'           => 'required|uuid|unique:users,uuid',
-            'nik'            => 'required|string|max:50|unique:users,nik',
-            'username'       => 'required|string|max:50',
-            'role'       => 'required|string|max:50',
-            'name'           => 'required|string|max:255',
-            'phone'          => 'required|string|max:20',
-            'email'          => 'required|string|email|max:255|unique:users,email',
-            'password'       => 'required|string|min:8',
+            'uuid'     => 'required|uuid|unique:users,uuid',
+            'nik'      => 'required|string|max:50|unique:users,nik',
+            'username' => 'required|string|max:50',
+            'role'     => 'nullable|string|max:50',
+            'name'     => 'required|string|max:255',
+            'phone'    => 'required|string|max:20',
+            'email'    => 'required|string|email|max:255|unique:users,email',
+            'password' => [
+                'required',
+                'string',
+                'min:8',
+                'regex:/^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\W_]).+$/',
+                function ($attribute, $value, $fail) {
+                    if (! preg_match('/^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\W_]).+$/', $value)) {
+                        return $fail('The ' . $attribute . ' must be at least 8 characters long and include at least one letter, one number, and one special character.');
+                    }
+                },
+            ],
+
         ]);
 
         if ($validator->fails()) {

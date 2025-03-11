c365d6f3f84fc106c2eae9fdde81e296295139b6
intisuks <intisuksesdevelopment@gmail.com>
2025-03-12 05:54:46 +0700

    [ADD][FEATURE][ORDER]add

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -1,7 +1,7 @@
 <?php
 namespace App\Services;
 
-use App\Models\Bank;
+use App\Models\Order;
 use Illuminate\Http\Request;
 use App\Constants\CommonConstants;
 use Illuminate\Support\AlreadyExistException;
@@ -9,5 +9,18 @@ use Illuminate\Support\Facades\Log;
 
 class OrderService
 {
-
+    public static function getPaginated(Request $request)
+    {
+        $perPage = $request->input('per_page', CommonConstants::PAGE);
+        // Default to 10 per page if not provided
+        $sortBy = $request->input('sortBy', CommonConstants::SORT);
+        // Default to 'id' if not provided
+        $sortDirection = $request->input('sortDirection', CommonConstants::DIRECTION);
+        // Default to 'asc' if not provided
+        $orders = Order::orderBy($sortBy, $sortDirection)->paginate($perPage);
+        foreach ($orders as $order) {
+            $order->availability = $order->isAvailable();
+        }
+        return $orders;
+    }
 }

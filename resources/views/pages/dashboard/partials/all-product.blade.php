196927dc9dc55c2c6cced659f2b89cd4e7501198
reza <rezadionugraha@gmail.com>
2025-05-21 20:32:56 +0700

    fixing

--- resources/views/pages/dashboard/partials/all-product.blade.php
+++ resources/views/pages/dashboard/partials/all-product.blade.php
@@ -85,6 +85,19 @@
 		cursor: pointer;
 		}
     </style>
+    <!-- breadcrumb-section -->
+	<div class="breadcrumb-section breadcrumb-bg">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 offset-lg-2 text-center">
+					<div class="breadcrumb-text">
+						<h2>PRODUCTS</h2>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- end breadcrumb section -->
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-3 sidebar">
@@ -217,6 +230,6 @@
                     list.classList.add('collapse-manual');
                 });
             });
-            
+
         });
     </script>
